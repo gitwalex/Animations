9b478763cde36ae807e5d747c03727c2bcdcc179
gerwalex <gerwalex@gmail.com>
2022-04-19 12:51:03 +0200

    Initial commit

---
+++ Animations/src/main/java/com/gerwalex/animations/view/Combinable.java
@@ -0,0 +1,21 @@
+package com.gerwalex.animations.view;
+
+import android.animation.AnimatorSet;
+import android.animation.TimeInterpolator;
+
+/**
+ * This interface is implemented only by animation classes that can be combined
+ * to animate together.
+ * 
+ */
+public interface Combinable {
+	
+	public void animate();
+	public AnimatorSet getAnimatorSet();
+	public Animation setInterpolator(TimeInterpolator interpolator);
+	public long getDuration();
+	public Animation setDuration(long duration);
+	public Animation setListener(
+            AnimationListener listener);
+
+}
