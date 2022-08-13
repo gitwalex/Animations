1b8b85e1d670a14d1a884a59032a85c17f05be06
gerwalex <gerwalex@gmail.com>
2022-08-13 13:53:23 +0200

    33 ViewPagerTransformer added

---
+++ Animations/src/main/java/com/gerwalex/animations/viewpager2/DefaultTransformer.java
@@ -0,0 +1,13 @@
+package com.gerwalex.animations.viewpager2;
+
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.viewpager2.widget.ViewPager2;
+
+public class DefaultTransformer implements ViewPager2.PageTransformer {
+    @Override
+    public void transformPage(@NonNull View page, float position) {
+        //nothing
+    }
+}
---
+++ Animations/src/main/java/com/gerwalex/animations/viewpager2/FadeOutTransformer.java
@@ -0,0 +1,14 @@
+package com.gerwalex.animations.viewpager2;
+
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.viewpager2.widget.ViewPager2;
+
+public class FadeOutTransformer implements ViewPager2.PageTransformer {
+    @Override
+    public void transformPage(@NonNull View page, float position) {
+        page.setTranslationX(-position * page.getWidth());
+        page.setAlpha(1 - Math.abs(position));
+    }
+}
